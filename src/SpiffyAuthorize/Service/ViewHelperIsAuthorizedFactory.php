372fbbb5dad0a5d151fa698d1a317a0b38cf9c15
Kyle Spraggs <theman@spiffyjr.me>
2013-09-09 12:54:42 -0500

    Fixed locator in view helper factory (need tests!).

--- src/SpiffyAuthorize/Service/ViewHelperIsAuthorizedFactory.php
+++ src/SpiffyAuthorize/Service/ViewHelperIsAuthorizedFactory.php
@@ -16,9 +16,12 @@ class ViewHelperIsAuthorizedFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        /** @var \Zend\View\HelperPluginManager $serviceLocator */
+        $sl = $serviceLocator->getServiceLocator();
+
         /** @var \SpiffyAuthorize\ModuleOptions $options */
-        $options = $serviceLocator->get('SpiffyAuthorize\ModuleOptions');
-        $service = $serviceLocator->get($options->getAuthorizeService());
+        $options = $sl->get('SpiffyAuthorize\ModuleOptions');
+        $service = $sl->get($options->getAuthorizeService());
 
         return new IsAuthorizedHelper($service);
     }
